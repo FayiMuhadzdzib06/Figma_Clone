25c57e9d90f4c9de99ee6f765d538bb887486d86
Fayi Muhadzdzib <fayimujahid1@gmail.com>
2022-07-04 13:22:56 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Thirty
+function favoritefile() {
+    document.getElementById('text').style.display = 'block';
+    document.getElementById('triangle').style.transform = 'rotate(180deg)';
+    document.getElementById('trans').style.display = 'block';
+}
+function trans() {
+    document.getElementById('text').style.display = 'none';
+    document.getElementById('triangle').style.transform = 'rotate(90deg)';
+    document.getElementById('trans').style.display = 'none';
+}
+
+// fourty
+function teams() {
+    document.getElementById('teams').style.display = 'block';
+    document.getElementById('trifourty').style.transform = 'rotate(180deg)';
+    document.getElementById('tream').style.display = 'block';
+}
+function transteams() {
+    document.getElementById('teams').style.display = 'none';
+    document.getElementById('trifourty').style.transform = 'rotate(90deg)';
+    document.getElementById('tream').style.display = 'none';
+}
